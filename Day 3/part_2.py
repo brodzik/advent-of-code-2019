ed9e0d1a94f09e72de572c4535bd439a0c4eafec
Adrian Brodzik <adrianbrodzyk@gmail.com>
2019-12-05 23:01:43 +0100

    Add day 3 part 2

---
+++ Day 3/part_2.py
@@ -0,0 +1,54 @@
+def main():
+    points = []
+
+    with open("input.txt") as input_file:
+        for wire in input_file:
+            wire = [(x[0], int(x[1:])) for x in wire.split(",")]
+
+            x, y = 0, 0
+            cost = 0
+            points_local = [(x, y, cost)]
+
+            for direction, value in wire:
+                if direction == "U":
+                    y += value
+                elif direction == "D":
+                    y -= value
+                elif direction == "R":
+                    x += value
+                elif direction == "L":
+                    x -= value
+                else:
+                    raise Exception("Invalid direction: " + direction)
+
+                cost += value
+                points_local.append((x, y, cost))
+
+            points.append(points_local)
+
+    results = []
+
+    for i in range(len(points[0]) - 1):
+        px1, py1 = points[0][i][0], points[0][i][1]
+        px2, py2 = points[0][i + 1][0], points[0][i + 1][1]
+
+        for j in range(len(points[1]) - 1):
+            qx1, qy1 = points[1][j][0], points[1][j][1]
+            qx2, qy2 = points[1][j + 1][0], points[1][j + 1][1]
+
+            if px1 == px2 and qy1 == qy2:
+                if px1 <= max(qx1, qx2) and px1 >= min(qx1, qx2) and px2 <= max(qx1, qx2) and px2 >= min(qx1, qx2):
+                    if max(py1, py2) >= qy1 and min(py1, py2) <= qy2:
+                        results.append(points[0][i][2] + points[1][j][2] + abs(px1 - qx1) + abs(qy1 - py1))
+                        #print((px1, qy1))
+            elif py1 == py2 and qx1 == qx2:
+                if qx1 <= max(px1, px2) and qx1 >= min(px1, px2) and qx2 <= max(px1, px2) and qx2 >= min(px1, px2):
+                    if max(qy1, qy2) >= py1 and min(qy1, qy2) <= py2:
+                        results.append(points[0][i][2] + points[1][j][2] + abs(qx1 - px1) + abs(py1 - qy1))
+                        #print((qx1, py1))
+
+    print(results)
+
+
+if __name__ == "__main__":
+    main()
